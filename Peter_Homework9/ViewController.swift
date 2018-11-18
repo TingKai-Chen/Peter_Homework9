cd6304e7a8c49f9b820efd705987fb760623c38a
“chentingkai” <bill800123@gmail.com>
2018-11-18 19:08:12 +0800

    Initial Commit

---
+++ Peter_Homework9/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Peter_Homework9
+//
+//  Created by 陳庭楷 on 2018/11/18.
+//  Copyright © 2018年 陳庭楷. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
